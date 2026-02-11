b049e622b9fe0af65dd92db2c761e63ff6948d52
mog-ttw <a.aiedogbo@alustudent.com>
2026-02-12 00:28:30 +0200

    h

--- tests/tests/test_console.py
+++ tests/tests/test_console.py
@@ -3,11 +3,15 @@
 
 import unittest
 
+
 class TestConsole(unittest.TestCase):
+
     """Basic placeholder tests for console"""
 
     def test_placeholder(self):
         self.assertTrue(True)
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
+
     unittest.main()
